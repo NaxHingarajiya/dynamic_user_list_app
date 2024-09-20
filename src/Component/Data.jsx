5408207f17e115ed6aaf9c1f82201b885d7e3cba
NaxHingarajiya <159748494+NaxHingarajiya@users.noreply.github.com>
2024-09-20 12:22:46 +0530

    Add files via upload

---
+++ src/Component/Data.jsx
@@ -0,0 +1,82 @@
+const Data=[
+    {
+        "id":1,
+        "photo":"https://i.pinimg.com/736x/33/ba/df/33badf7bd7e2bd56b21e3d972fe3ed5a.jpg",
+        "name":"Nax",
+        "age":20,
+        "address":"Umiya Circle",
+        "phone":"96627 25041"
+
+    },
+    {
+        "id":2,
+        "photo":"https://i.pinimg.com/736x/33/ba/df/33badf7bd7e2bd56b21e3d972fe3ed5a.jpg",
+        "name":"Anup",
+        "age":21,
+        "address":"Indira Circle",
+        "phone":"96627 25041"
+
+    },
+
+    {
+        "id":3,
+        "photo":"https://i.pinimg.com/736x/33/ba/df/33badf7bd7e2bd56b21e3d972fe3ed5a.jpg",
+        "name":"krishna",
+        "age":20,
+        "address":"West Zone",
+        "phone":"96627 25041"
+
+    },
+
+    {
+        "id":4,
+        "photo":"https://i.pinimg.com/736x/33/ba/df/33badf7bd7e2bd56b21e3d972fe3ed5a.jpg",
+        "name":"Dhaval",
+        "age":18,
+        "address":"Nana Mauva",
+        "phone":"96627 25041"
+
+    },
+
+    {
+        "id":5,
+        "photo":"https://i.pinimg.com/736x/33/ba/df/33badf7bd7e2bd56b21e3d972fe3ed5a.jpg",
+        "name":"Preet",
+        "age":24,
+        "address":"KKV Circle",
+        "phone":"9998003879"
+
+    },
+
+    {
+        "id":6,
+        "photo":"https://i.pinimg.com/736x/33/ba/df/33badf7bd7e2bd56b21e3d972fe3ed5a.jpg",
+        "name":"Yash",
+        "age":23,
+        "address":"Balaji Cirle",
+        "phone":"96627 25041"
+
+    },
+
+    {
+        "id":7,
+        "photo":"https://i.pinimg.com/736x/33/ba/df/33badf7bd7e2bd56b21e3d972fe3ed5a.jpg",
+        "name":"Sahil",
+        "age":22,
+        "address":"Gondal Circle",
+        "phone":"96627 25041"
+
+    },
+
+    {
+        "id":8,
+        "photo":"https://i.pinimg.com/736x/33/ba/df/33badf7bd7e2bd56b21e3d972fe3ed5a.jpg",
+        "name":"Shubham",
+        "age":21,
+        "address":"Raiya Circle",
+        "phone":"96627 25041"
+
+    }
+]
+
+export default Data
